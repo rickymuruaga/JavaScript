3344fecc9d14081f949877b4b2ba490ef639cfd6
Ricardo Muruaga <richardmuru25@gmail.com>
2023-04-20 12:41:32 -0300

    ejercitando after

---
+++ ejecicios/main.js
@@ -0,0 +1,33 @@
+// let nombre = "Ricky";
+// let apellido = "Muruaga";
+// let edad = 28; (POR QUE EL NUMERO EN VARIABLE EDAD VA SIN COMILLAS??????)
+
+// console.log(nombre);
+// console.log(apellido);
+// console.log(edad);
+
+/*
+   CONSTANTES: A LA VARIABLE SE LE ASIGNA UN NUMERO A CADA UNA
+
+// const ciudad1 = "San Miguel de Tucuman";
+// const ciudad2 = "Buenos Aires";
+// const ciudad3 = "Madrid";
+// const ciudad4 = "Coppenhagen";
+// const ciudad5 = "Brujas";
+
+// console.log(ciudad1);
+// console.log(ciudad2);
+// console.log(ciudad3);
+// console.log(ciudad4);
+// console.log(ciudad5);    
+
+*/
+
+let nombre = "Ricky";
+let apellido = "Muruaga";
+let direccion = "Tucuman"
+let pais = "Argentina";
+
+console.log(``);
+
+ 
