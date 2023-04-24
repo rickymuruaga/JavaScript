6d5f740307e657d625227e1fc285c907ab28bc4e
Ricardo Muruaga <richardmuru25@gmail.com>
2023-04-24 11:50:58 -0300

    clase4

---
+++ Clase 4/main.js
@@ -0,0 +1,9 @@
+// function saludar (saludo, nombre){
+//     let mensaje = `${saludo}, ${nombre}`;
+//     console.log(mensaje); 
+// }
+// saludar("Bienvenido", "Ricky");
+
+function sumar(numero1, numero2){
+    return numero1  + numero2;
+}
