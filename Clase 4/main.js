76a3c1ea4190cf7373a95f32706557e3d977cecf
Ricardo Muruaga <richardmuru25@gmail.com>
2023-04-25 20:23:24 -0300

    practicando

--- Clase 4/main.js
+++ Clase 4/main.js
@@ -1,10 +1,56 @@
-// function saludar (saludo, nombre){
-//     let mensaje = `${saludo}, ${nombre}`;
-//     console.log(mensaje); 
-// }
-// saludar("Bienvenido", "Ricky");
-
-function info(nombre, edad){
-    return 
+/* 
+estructura de una funcion
+function nombreFuncion(){
+  codigo a ejecutar por la funcion
 }
-console.log();
+*/
+
+// function saludar (){
+//     console.log("Hola");
+// }
+
+// saludar();
+
+/*ESTRUCTURA DE LAS FUNCIONES CON PARAMETROS
+   EJ. FUNCION "SALUDAR" Y LO QUE VA ENTRE PARENTESIS SON LOS PARAMETROS SEPARADOS POR UNA COMA()
+   ENTRE LLAVES {LA FUNCION A EJECUTAR} 
+*/
+
+// function multiplicacion (num1,num2){
+//     console.log(num1*num2);
+// }
+
+// multiplicacion(2, 4)
+
+function calculadora(num1,num2,operacion){
+ switch(operacion){
+      case "+":
+       return num1+num2;
+        break;
+
+    case "-":
+      return num1-num2;
+       break;
+    
+    case "*":
+     return num1*num2;
+     break;
+    
+    case "/":
+     return num1/num2;
+     break;
+    
+    default:
+     return "operacion invalida";
+     break;
+
+    }
+}
+
+let num1 = parseInt(prompt ("ingrese numero uno"));
+let operacion = prompt("ingrese operacion");
+let num2 = parseInt(prompt("ingrese numero 2"));
+
+
+let resultado = calculadora(num1, num2, operacion);
+alert(`el resultado de ${num1} ${operacion} ${num2} = ${resultado}`);
