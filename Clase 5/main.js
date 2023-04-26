c462b215bc5f9f176ca68240db78aaa8bc534a43
Ricardo Muruaga <richardmuru25@gmail.com>
2023-04-26 18:03:21 -0300

    clase 5

---
+++ Clase 5/main.js
@@ -0,0 +1,28 @@
+/* const persona ={
+    nombre : "Ricky",
+    edad : 28,
+    ciudad : "Tucuman",           
+};
+                               DOS FORMAS DIFERENTES DE LLAMAR PARAMETROS
+console.log(persona.nombre);
+console.log(persona["nombre"]);
+ */
+
+/*  function Persona(nombre, edad, direccion){
+  this.nombre = nombre;
+   this.edad = edad;
+   this.direccion = direccion;
+ }
+
+ let persona1 = new Persona("Ricky", 45, "AV");
+ let persona2 = new Persona("Muruaga", 46, "AV1");
+ console.log(persona1);
+ console.log(persona2);
+
+  */
+
+ let persona1 = new persona("Ricky", 28, "Tucuman");
+ let persona2 = new persona("andres", 25, "colombia");
+
+ console.log(persona1);
+ console.log(persona2);
